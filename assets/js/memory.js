2a389577b845001981b6494c5f755b75544eb3c8
AlienatedSoftware <aidancgart@gmail.com>
2021-05-23 16:23:01 +0000

    setup card array for memory.html

---
+++ assets/js/memory.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    //Cards
+    const cardArray = [
+        {
+            name: 'game console',
+            img: 'assets/memory/game-console.png'
+        },
+        {
+            name: 'game console',
+            img: 'assets/memory/game-console.png'
+        },
+        {
+            name: 'gamer',
+            img: 'assets/memory/gamer.png'
+        },
+        {
+            name: 'gamer',
+            img: 'assets/memory/gamer.png'
+        },
+        {
+            name: 'ghost',
+            img: 'assets/memory/ghost.png'
+        },
+        {
+            name: 'ghost',
+            img: 'assets/memory/ghost.png'
+        },
+        {
+            name: 'hand held',
+            img: 'assets/memory/hand-held.png'
+        },
+        {
+            name: 'hand held',
+            img: 'assets/memory/hand-held.png'
+        },
+        {
+            name: 'heart',
+            img: 'assets/memory/heart.png'
+        },
+        {
+            name: 'heart',
+            img: 'assets/memory/heart.png'
+        },
+        {
+            name: 'rubik',
+            img: 'assets/memory/rubik.png'
+        },
+        {
+            name: 'rubik',
+            img: 'assets/memory/rubik.png'
+        },
+    ]
+})

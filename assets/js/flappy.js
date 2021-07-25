70fc25c45e73feb7ca383c4ba2f71d0ec444169c
AlienatedSoftware <aidancgart@gmail.com>
2021-07-25 08:55:52 +0000

    added comments to .js code for flappy game

--- assets/js/flappy.js
+++ assets/js/flappy.js
@@ -4,36 +4,42 @@ var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
 
+/*----Random generation of holes in obstacles function----*/
 hole.addEventListener('animationiteration', () => {
     var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
     counter++;
 });
 
+/*----Gravity function----*/
 setInterval(function() {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0) {
-        character.style.top = (characterTop+3)+"px";
+        character.style.top = (characterTop+2)+"px";
     }
+    /*----Hitboxes----*/
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     var cTop = -(500-characterTop);
+    /*----Collision function & score counter alert----*/
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))) {
         alert("Score: "+(counter-1));
-        character.style.top = 100 + "px";
+        character.style.top = 115 + "px";
         counter=0;
     }
 },10);
 
+/*----Jumping function----*/
 function jump() {
     jumping = 1;
+    /*----Function to allow gravity to kick in after jump----*/
     let jumpCount = 0;
     var jumpInterval = setInterval(function(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-        if((characterTop>6)&&(jumpCount<15)){
-            character.style.top = (characterTop-5)+"px";
+        if((characterTop>2)&&(jumpCount<15)){
+            character.style.top = (characterTop-4)+"px";
         }
-        if(jumpCount>20){
+        if(jumpCount>15){
             clearInterval(jumpInterval);
             jumping=0;
             jumpCount=0;
